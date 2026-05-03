fdf4b95971d0b3dc73ea592b059e5f2f778f73f5
Jared Onguso <jonguso@gmail.com>
2026-05-02 19:05:13 -0500

    Prepare React frontend for Railway

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,5 @@
 import axios from "axios";
-export default axios.create({ baseURL: "http://localhost:4000" });
+
+export default axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000"
+});
